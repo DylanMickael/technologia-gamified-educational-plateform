ae2340f97cd19a85da76aab5ff6c34a4c65f317f
dorissylvie <vololonirinadorissylvie@gmail.com>
2025-06-14 22:23:41 +0300

    feat:Register Page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,28 +2,29 @@ import Faciale from "./authentification/faciale";
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Aos from 'aos';
+import Aos from "aos";
 import "aos/dist/aos.css";
+import Register from "./pages/register";
 
 function App() {
-
   useEffect(() => {
-      Aos.init({
-          duration: 800,
-          easing: 'ease-out',
-          offset: 50, 
-          once: false
-      });
+    Aos.init({
+      duration: 800,
+      easing: "ease-out",
+      offset: 50,
+      once: false,
+    });
   }, []);
 
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path = "/faciale" element = {<Faciale/>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/faciale" element={<Faciale />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- front/src/pages/Landing/index.tsx
+++ front/src/pages/Landing/index.tsx
@@ -1,22 +1,21 @@
-import LandingLayout from '../..//layouts/LandingLayout';
-import HeroSection from './components/HeroSection';
-import AboutSection from './components/AboutSection';
-import BannerSection from './components/BannerSection';
-import ContactSection from './components/ContactSection';
-import AnimatedCursor from '../../components/AnimatedCursor';
-import butterflyCursor from '../../assets/cursors/Butterfly.ani';
-import birdCursor from '../../assets/cursors/Bird.ani';
+import LandingLayout from "../..//layouts/LandingLayout";
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+import BannerSection from "./components/BannerSection";
+import ContactSection from "./components/ContactSection";
+import AnimatedCursor from "../../components/AnimatedCursor";
+import butterflyCursor from "../../assets/cursors/Butterfly.ani";
+import birdCursor from "../../assets/cursors/Bird.ani";
 
 const Landing = () => {
-
   return (
     <LandingLayout>
-      <HeroSection/>
-      <AboutSection/>
-      <BannerSection/>
-      <ContactSection/>      
-      <AnimatedCursor 
-        selector=".animated-cursor" 
+      <HeroSection />
+      <AboutSection />
+      <BannerSection />
+      <ContactSection />
+      <AnimatedCursor
+        selector=".animated-cursor"
         cursor={butterflyCursor}
         darkCursor={birdCursor}
       />
